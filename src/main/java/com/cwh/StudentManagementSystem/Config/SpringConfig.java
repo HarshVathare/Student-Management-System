6ccd8c7437d6f2667d7cd62abc3be49edaae5c70
HarshVathare <harsh1234vathare@gmail.com>
2026-01-24 00:40:51 +0530

    add user service class for spring security

--- src/main/java/com/cwh/StudentManagementSystem/Config/SpringConfig.java
+++ src/main/java/com/cwh/StudentManagementSystem/Config/SpringConfig.java
@@ -2,13 +2,30 @@ package com.cwh.StudentManagementSystem.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SpringConfig {
+
+    private static final String[] PUBLIC_PATH = {
+          "/login",
+          "/signup"
+    };
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        httpSecurity.authorizeHttpRequests(auth->auth
+                .requestMatchers(PUBLIC_PATH).permitAll()
+                .anyRequest().authenticated()
+        );
+        return httpSecurity.build();
+    }
+
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
---
+++ src/main/java/com/cwh/StudentManagementSystem/Services/Impl/UserService.java
@@ -0,0 +1,9 @@
+package com.cwh.StudentManagementSystem.Services;
+
+/************************************************************************
+  Developer Name : Harshvardhan Vathare
+  Developer Content : harsh1234vathare@gmail.com
+  Created On : 1/23/2026 11:53 PM
+  Project Name : StudentManagementSystem
+ ************************************************************************/public class UserService {
+}
